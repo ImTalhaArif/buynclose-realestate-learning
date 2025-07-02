acdb39d2e58e8d850ffd1284e1345b37e48f0182
ImTalhaArif <62965957+ImTalhaArif@users.noreply.github.com>
2025-07-03 03:19:36 +0500

    Update page.tsx

--- app/student-dashboard/chapters/page.tsx
+++ app/student-dashboard/chapters/page.tsx
@@ -1,7 +1,6 @@
 'use client';
-
-import Header from '../pages/components/Header';
-import Footer from '../pages/components/Footer';
+import Header from '../../pages/components/Header';
+import Footer from '../../pages/components/Footer';
 
 export default function ChapterPage() {
   return (
